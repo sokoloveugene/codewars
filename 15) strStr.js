632190c2e12aac32c02ead8033824e2a4ea5dc76
Yevhenii <forallapiacc@gmail.com>
2021-06-15 21:38:31 +0300

    strStr

---
+++ 15) strStr.js
@@ -0,0 +1,14 @@
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+
+// Example 2:
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
+
+var strStr = function (haystack, needle) {
+  if (needle.length === 0) return 0;
+  return haystack.indexOf(needle);
+};
